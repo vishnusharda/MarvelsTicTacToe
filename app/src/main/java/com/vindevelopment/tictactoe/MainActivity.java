e8d9a8c5fa52cc79200e1ec68e994abf3df08acf
vishnu sharda <vishnusharda1234@gmail.com>
2021-01-10 22:30:03 +0530

    Marvels Tic Tac Toe

--- app/src/main/java/com/vindevelopment/tictactoe/MainActivity.java
+++ app/src/main/java/com/vindevelopment/tictactoe/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView winner_tv;
     Button play_again;
     int active;
+    int i;
     int[] position = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
